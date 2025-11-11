460354ae491691749b51075a9dde88defedca852
Cristian <calarcon@fi.uba.ar>
2025-11-11 20:18:54 -0300

    se agrega un div para la el manejo de el titulo y demas menos el footer, se suma un footer para la card y posteriormente usar el id para redireccion, se usaron estilo en modulo; mismo funcionamiento

--- src/components/EmpleoCard.jsx
+++ src/components/EmpleoCard.jsx
@@ -1,31 +1,36 @@
 import React from 'react'
-
+import styles from './EmpleoCard.module.css'
 const EmpleoCard = (props) => {
     /* Revisar si esto mejor llevarlo a otro lado, desestructure todo*/
     const {titulo, empresa, ubicacion, salario, descripcion,
-        modalidad, seniority}=props.dataEmpleo;
+        modalidad, seniority, id}=props.dataEmpleo;
     
 
-  return (
-    <article>
-        <section>
-            <h3>{titulo}</h3>
-            <p><strong>{empresa}</strong></p>
-        </section>
-        <section>
-            <p>{descripcion}</p>
-        </section>
-        <section>
-            <ul>
-            <li>Ubicacion: {ubicacion}</li>
-            <li>Salario:{salario}</li>
-            <li>Modalidad: {modalidad}</li>
-            <li>Seniority:{seniority}</li>
-            </ul>
-        </section>
-        
-    </article>
-  )
+return (
+    <article className={styles.card}>
+            <div className={styles.content}>
+                <header className={styles.header}>
+                    <h3 className={styles.title}>{titulo}</h3>
+                    
+                    <p className={styles.subtitle}>{empresa} - {modalidad}</p>
+                </header>
+                <p className={styles.description}>{descripcion}</p>
+                <ul className={styles.metadata}>
+                    <li>{ubicacion}</li>
+                    <li>{salario}</li>
+                    <li>{seniority}</li>
+                </ul>
+            </div>
+            <footer className={styles.footer}>
+                <a 
+                    href={`/empleos/${id}`} 
+                    className={styles.button}
+                >
+                    Ver oferta
+                </a>
+            </footer>
+        </article>
+        )
 }
 
 export default EmpleoCard
