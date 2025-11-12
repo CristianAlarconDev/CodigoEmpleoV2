bfebb98207d51328205a50c88820d071db73c177
Ariel <ariel.oliva3748@gmail.com>
2025-11-12 13:59:02 -0300

    Creo la cardCurso y el contendor(modifico cursosPage) con sus estilos en ReactBoostrap. Hace falta agregar filtros

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Rutas from './routes/Rutas';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 const App = () => {
   return (
    
