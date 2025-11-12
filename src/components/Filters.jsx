038a9f59bfcaa2a6ccad03b28d1af89cb09d2404
Cristian <calarcon@fi.uba.ar>
2025-11-12 11:28:08 -0300

    se suma div para controlar el input/label dentro de modalidads

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -29,7 +29,7 @@ const Filters = ({filtrosSeleccionados,onFiltroChange}) => {
     <section className={styles.container}>
         <h2 className={styles.title}>Filtros</h2>
         <div className={styles.formGroup}>
-          <label htmlFor="seniority-select"> Seniority</label>
+          <label htmlFor="seniority-select" className={styles.legend}> Seniority</label>
           <select className={styles.select} name="seniority" id="seniority-select" onChange={onFiltroChange} value={filtrosSeleccionados.seniority}>
             <option value="todos">Todos los niveles</option>
             <option value="Trainee">Trainee</option>
@@ -40,13 +40,19 @@ const Filters = ({filtrosSeleccionados,onFiltroChange}) => {
         </div>
         
         <fieldset className={styles.fieldsetGroup}>
-            <legend className={styles.legend}>Modalidad</legend>
+          <legend className={styles.legend}>Modalidad</legend>
+          <div className={styles.checkboxWrapper}>
             <input className={styles.checkbox} type="checkbox" value="remoto" id="modalidad-remoto" name='remoto' onChange={onFiltroChange} checked={filtrosSeleccionados.remoto}/>
             <label htmlFor="modalidad-remoto">Remoto</label>
+          </div>
+          <div className={styles.checkboxWrapper}>
             <input className={styles.checkbox} type="checkbox" value="hibrido"id="modalidad-hibrido" name='hibrido' onChange={onFiltroChange}checked={filtrosSeleccionados.hibrido}/>
             <label htmlFor="modalidad-hibrido">Hibrido</label>
+          </div>
+          <div className={styles.checkboxWrapper}>
             <input className={styles.checkbox} type="checkbox" value="presencial"id="modalidad-presencial" name='presencial' onChange={onFiltroChange}checked={filtrosSeleccionados.presencial}/>
             <label htmlFor="modalidad-presencial" >Presencial</label>
+          </div>
         </fieldset>
     </section>
   )
