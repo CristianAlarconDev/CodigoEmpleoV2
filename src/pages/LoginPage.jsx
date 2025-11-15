af2874077f1ca697ed04bf62faca60b9ca563d1d
Cristian <calarcon@fi.uba.ar>
2025-11-15 14:30:01 -0300

    se msuma estructura base de login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 
 const LoginPage = () => {
+
+    const googleLogin = () => {
+        // Lógica para iniciar sesión con Google
+        console.log("Iniciar sesión con Google");
+    }
+
+
+
   return (
-    <div>LoginPage</div>
+    <section>
+        <section>
+            <h2>Iniciar Sesion</h2>
+            <p>Inicia sesión para guardar tus postulaciones.</p>
+        </section>
+        
+        <section>
+            <button type='button' onClick={googleLogin}>
+                Continuar con Google
+            </button>
+        </section>
+            
+    </section>
   )
 }
 
