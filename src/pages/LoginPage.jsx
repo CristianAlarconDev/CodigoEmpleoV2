984e491f6405e863a795ace7df9e89d8c4e7e368
Cristian <calarcon@fi.uba.ar>
2025-11-16 15:03:17 -0300

    primera version de login page, se penso como modal pero por ahora se queda en page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,29 +1,63 @@
 import React from 'react'
+import { signInWithPopup } from "firebase/auth";
+import { autenticacion, googleProvider } from '../config/firebase';
 
 const LoginPage = () => {
-
-    const googleLogin = () => {
-        // Lógica para iniciar sesión con Google
-        console.log("Iniciar sesión con Google");
-    }
-
-
-
-  return (
-    <section>
-        <section>
-            <h2>Iniciar Sesion</h2>
-            <p>Inicia sesión para guardar tus postulaciones.</p>
-        </section>
+    const handleGoogleLogin = async () => {
+    try {
+        const result = await signInWithPopup(autenticacion, googleProvider);
         
-        <section>
-            <button type='button' onClick={googleLogin}>
+        console.log("Usuario logueado!");
+        
+        console.log("Nombre:", result.user.displayName);
+        console.log("Email:", result.user.email);
+        console.log("Foto URL:", result.user.photoURL);
+        console.log("UID:", result.user.uid); 
+
+        // Aca deberia redirigir o hacer algo tras el login,
+        // como guardalo en un contexto
+
+        } catch (error) {
+        // manejar errores aca:
+        console.error("Error durante el inicio de sesión:", error.message);
+    }
+    };
+
+    return (
+
+        <section className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <section className="bg-white p-8 rounded-lg shadow-md text-center">
+            
+            {/* texto */}
+            <section className="mb-6"> 
+                <h2 className="text-2xl font-bold text-gray-800 mb-2">Iniciar Sesión</h2>
+                <p className="text-gray-600">Inicia sesión para guardar tus postulaciones.</p>
+            </section>
+            
+            {/* boton, cambiar estilos luego */}
+            <section>
+            <button 
+                type='button' 
+                onClick={handleGoogleLogin}
+                className="
+                flex items-center gap-2 mx-auto   
+                px-6 py-3                         
+                bg-white                           
+                rounded-lg                         
+                text-gray-700 font-medium        
+                hover:bg-gray-50                  
+                ">
+            
+                <img src="https://www.svgrepo.com/show/475656/google-color.svg" className="w-6 h-6" 
+                alt="Google logo" 
+                />
                 Continuar con Google
             </button>
+            </section>
+
+            </section>
         </section>
-            
-    </section>
-  )
+    )
 }
 
 export default LoginPage
