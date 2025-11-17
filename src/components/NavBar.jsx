5cbd0b26123963b36ba016e0c538ce7cb0ef5899
CristianAlarconDev <calarcon@fi.uba.ar>
2025-11-16 22:50:53 -0300

    se suma context de autenticaion en navbar, solo usa el nombre por ahora y se corrige error en alineacion de items del mismo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
+import { useAutenticacionContext } from '../context/AutenticacionContext';
 const NavBar = () => {
+  const { usuario,  logout } = useAutenticacionContext();
+
   const getStyle = ({ isActive }) => {
   
   return isActive 
@@ -12,7 +15,7 @@ const NavBar = () => {
     <nav className="bg-gray-800 text-white p-4">
       <div className="flex justify-between items-center">
         <h1 className="text-xl font-bold">MiMarca</h1>
-        <ul className="flex gap-4">
+        <ul className="flex items-center gap-4">
           {/* Link de Home */}
           <li>
             <NavLink to="/" className={getStyle} end>
@@ -35,11 +38,25 @@ const NavBar = () => {
               Recursos
             </NavLink>
           </li>
-          <li>
-            <NavLink to="/login" className={getStyle}>
-              Login
-            </NavLink>
-          </li>
+          
+          {usuario ? (
+            // SÍ: El usuario está logueado
+            <li>
+              <section className="flex items-center gap-4">
+              <span className="text-gray-300">Hola, {usuario.nombre}</span>
+              <button onClick={logout} className="text-white bg-red-600 p-2 rounded-md hover:bg-red-700">
+                Logout
+              </button>
+              </section>
+            </li>
+          ) : (
+            // NO: El usuario no está logueado
+            <li>
+              <NavLink to="/login" className={getStyle}>
+                Login
+              </NavLink>
+            </li>
+          )}
         </ul>
       </div>
     </nav>
