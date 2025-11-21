591cb1b60d6d0da285f62eedb09804c2114c05db
Cristian <calarcon@fi.uba.ar>
2025-11-21 19:37:08 -0300

    primera version de un home page; con splice se usa useFetch de cursos y empleos para poder mostrar lso primeros de ambas listas 'asumir los primeros son los ultimos por ahora'; funciona ok, luego sumar mas estilos o un hero component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { useFetch } from '../hooks/useFetch.js';
-import EmpleosList from '../components/EmpleosList.jsx';
+import CursoCard from '../components/CursoCard.jsx';
+import EmpleoCard from '../components/EmpleoCard.jsx';
 
 const HomePage = () => {
   const {data:empleos, cargando:cargandoEmpleos}=useFetch(import.meta.env.VITE_MOCKAPI_ENDPOINT_EMPLEOS);
   const empleosRecientes = empleos.slice(0, 4);
-  
+  const {data:cursos, cargando:cargandoCursos}=useFetch(import.meta.env.VITE_MOCKAPI_ENDPOINT_CURSOS);
+  const cursosRecientes = cursos.slice(0, 4);
+
 
   return (
     <div className="flex flex-col min-h-screen">
@@ -14,16 +17,31 @@ const HomePage = () => {
       <main className="grow container mx-auto px-4 py-8">
         {/*seccion para cursos recientes */}
         <section>
-          <h2>Cursos Recientes</h2>
-
+          <h2 >Cursos Recientes</h2>
+          {cargandoCursos? (
+                    <p>Cargando cursos...</p>
+                ) : (
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+                        {cursosRecientes.map((curso) => (
+                          <CursoCard unCurso={curso} />
+                        ))}
+                    </div>
+                    )
+          }
         </section>
         {/*seccion para empleos recientes */}
         <section>
           <h2>Ultimos empleos</h2>
-          {
-            cargandoEmpleos ? (
-              <p>Cargando empleos...</p>
-            ) : (<EmpleosList listaEmpleos={empleosRecientes} />)     
+          
+          {cargandoEmpleos? (
+                    <p>Cargando empleos...</p>
+                ) : (
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                      {empleosRecientes.map((unEmpleo) => (
+                          <EmpleoCard key={unEmpleo.id} dataEmpleo={unEmpleo}/>
+                      ))}
+                    </div>
+                    )
           }
         </section>
       </main>
