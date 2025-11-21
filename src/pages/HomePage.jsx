9b3da2189c65ee3ad1b73bf984ea3b611a3b0674
Cristian <calarcon@fi.uba.ar>
2025-11-21 17:58:55 -0300

    ahora homepage usa el hook useFetch con url de endpoint empleos, es var de entorno; mismo funcionamiento

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
-import { useEmpleos } from '../hooks/useEmpleos.js';
+import { useFetch } from '../hooks/useFetch.js';
 import EmpleosList from '../components/EmpleosList.jsx';
 
 const HomePage = () => {
-  const {empleos, cargando}=useEmpleos();
-  const empleosRecientes = empleos.slice(0, 4);
-
+  const {data, cargando}=useFetch(import.meta.env.VITE_MOCKAPI_ENDPOINT_EMPLEOS);
+  const empleosRecientes = data.slice(0, 4);
+  
 
   return (
     <div className="flex flex-col min-h-screen">
       
       
       <main className="grow container mx-auto px-4 py-8">
+        {/*seccion para cursos recientes */}
+        <section>
+          <h2>Cursos Recientes</h2>
+
+        </section>
         {/*seccion para empleos recientes */}
         <section>
           <h2>Ultimos empleos</h2>
