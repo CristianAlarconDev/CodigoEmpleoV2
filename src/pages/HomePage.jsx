52663a4ea78232a04755965ad84df4e25cb7faf1
Cristian <calarcon@fi.uba.ar>
2025-11-21 19:51:23 -0300

    se suma contenedor principal para cursos y empleos con margin horizontal auto, espaciado vertical ; luego el contenedor raiz que va a tener el hero y los demas es flex con un min height en unidad de pantalla vh para intentar usar todo el height del dispositivo

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,41 +12,42 @@ const HomePage = () => {
 
   return (
     <div className="flex flex-col min-h-screen">
-      
-      
-      <main className="grow container mx-auto px-4 py-8">
+      <main className="grow py-12 px-4">
+        <div className="max-w-7xl mx-auto space-y-16">
         {/*seccion para cursos recientes */}
-        <section>
-          <h2 >Cursos Recientes</h2>
-          {cargandoCursos? (
-                    <p>Cargando cursos...</p>
-                ) : (
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
-                        {cursosRecientes.map((curso) => (
-                          <CursoCard unCurso={curso} />
+          <section>
+            <h2 className=
+            "text-3xl font-bold text-gray-900 mb-8 border-l-4 border-blue-600 pl-4">
+              Cursos Recientes</h2>
+            {cargandoCursos? (
+                      <p>Cargando cursos...</p>
+                  ) : (
+                      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+                          {cursosRecientes.map((curso) => (
+                            <CursoCard unCurso={curso} />
+                          ))}
+                      </div>
+                      )
+            }
+          </section>
+          {/*seccion para empleos recientes */}
+          <section>
+            <h2 className=
+            "text-3xl font-bold text-gray-900 mb-8 border-l-4 border-blue-600 pl-4">
+              Ultimos empleos</h2>  
+            {cargandoEmpleos? (
+                      <p>Cargando empleos...</p>
+                  ) : (
+                      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                        {empleosRecientes.map((unEmpleo) => (
+                            <EmpleoCard key={unEmpleo.id} dataEmpleo={unEmpleo}/>
                         ))}
-                    </div>
-                    )
-          }
-        </section>
-        {/*seccion para empleos recientes */}
-        <section>
-          <h2>Ultimos empleos</h2>
-          
-          {cargandoEmpleos? (
-                    <p>Cargando empleos...</p>
-                ) : (
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                      {empleosRecientes.map((unEmpleo) => (
-                          <EmpleoCard key={unEmpleo.id} dataEmpleo={unEmpleo}/>
-                      ))}
-                    </div>
-                    )
-          }
-        </section>
+                      </div>
+                      )
+            }
+          </section>
+        </div>
       </main>
-
-      
     </div>
   )
 }
