371ab6a5f51ed0f8a073d47b37eeac9ca7361f40
CristianAlarconDev <calarcon@fi.uba.ar>
2025-12-07 13:49:32 -0300

    empleospage ahora usa un paginador con 10 cards por pagina, usa el componente paginador y le delega el setPaginaActual, luego deberia envolver el set en algun handler

--- src/pages/EmpleosPage.jsx
+++ src/pages/EmpleosPage.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import EmpleosList from '../components/EmpleosList.jsx';
 import Filters from '../components/Filters.jsx';
+import Paginador from '../components/Paginador';
 import { useFetch } from '../hooks/useFetch.js';
 import { useEmpleosFilters } from '../hooks/useEmpleosFilters.jsx';
 const EmpleosPage = () => {
@@ -8,6 +9,20 @@ const EmpleosPage = () => {
     const {data:empleos, cargando}=useFetch(import.meta.env.VITE_MOCKAPI_ENDPOINT_EMPLEOS);
   
     const { filtros, handleFilterChange, empleosFiltrados } = useEmpleosFilters(empleos);
+
+    const ITEMS_POR_PAGINA = 10;
+    const PRIMERA_PAGINA=1;
+    const [paginaActual, setPaginaActual] = useState(PRIMERA_PAGINA);
+
+    /*Hace de watcher, cuando filtros cambio se va a la pagina 1 */
+    useEffect(() => {
+        setPaginaActual(1);
+    }, [filtros]);
+
+    const indiceFinal = paginaActual * ITEMS_POR_PAGINA; 
+    const indiceInicial = indiceFinal - ITEMS_POR_PAGINA;
+    const empleosParaMostrar = empleosFiltrados.slice(indiceInicial, indiceFinal);
+    const totalPaginas = Math.ceil(empleosFiltrados.length / ITEMS_POR_PAGINA);
 
   return (
     <div className="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8">
@@ -20,8 +35,9 @@ const EmpleosPage = () => {
         <Filters filtrosSeleccionados={filtros} onFiltroChange={handleFilterChange}/>
       </div>
       <div className="lg:col-span-3">
-        {cargando? (<p>Buscando empleos...</p>):(<EmpleosList listaEmpleos={empleosFiltrados} />
+        {cargando? (<p>Buscando empleos...</p>):(<EmpleosList listaEmpleos={empleosParaMostrar} />
         )}
+        <Paginador paginaActual={paginaActual} totalPaginas={totalPaginas} onChangePagina={setPaginaActual}/>
       </div>
       </div>
       
