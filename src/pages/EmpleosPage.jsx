aa324d60e98fe66ee00e78d5bf4af4f7c44042e4
CristianAlarconDev <calarcon@fi.uba.ar>
2025-12-07 19:29:46 -0300

    se agrega componente searchbar para filtrar palabras claves se pasa el campo busqueda para mostrar y el handler de filters para poder 'accionarlo' cuando ingresemos el input en el searchbar

--- src/pages/EmpleosPage.jsx
+++ src/pages/EmpleosPage.jsx
@@ -1,6 +1,7 @@
 import EmpleosList from '../components/EmpleosList.jsx';
 import Filters from '../components/Filters.jsx';
 import Paginador from '../components/Paginador';
+import SearchBar from '../components/SearchBar';
 import { useFetch } from '../hooks/useFetch.js';
 import { useEmpleosFilters } from '../hooks/useEmpleosFilters.js';
 import { usePaginacion } from '../hooks/usePaginacion.js';
@@ -17,11 +18,17 @@ const EmpleosPage = () => {
       <h2 className="text-3xl font-bold text-gray-900 mb-8">
         Empleos 
       </h2>
+      <div className="bg-white p-4 rounded-lg shadow-sm border border-gray-100 mb-8">
+        <h3 className="text-xl font-bold text-gray-900 mb-6">Búsqueda</h3>
+        <SearchBar valorBusqueda={filtros.busqueda} onSearchChange={handleFilterChange} />
+      </div>
+
       {/*Cont principal para disposicion desktop o mobile, cambiar o llevar a css propio */}
       <div className="grid grid-cols-1 lg:grid-cols-4 gap-8 ">
         <div className="lg:col-span-1">
         <Filters filtrosSeleccionados={filtros} onFiltroChange={handleFilterChange}/>
-      </div>
+        </div>
+        
       <div className="lg:col-span-3">
         {cargando? (<p>Buscando empleos...</p>):(<EmpleosList listaEmpleos={empleosParaMostrar} />
         )}
