9ca2542a595bead27d74e74dab7a3996890e3c16
CristianAlarconDev <calarcon@fi.uba.ar>
2025-12-07 23:30:58 -0300

    se mueve el paginador fuera de la grilla para poder centrarlo horizontalmente, el search bar se mueve dentro de la grilla de cards asi ocupa ese ancho y se removio su titulo 'busqueda'

--- src/pages/CursosPage.jsx
+++ src/pages/CursosPage.jsx
@@ -28,20 +28,21 @@ return (
     <h2 className="text-3xl font-bold text-gray-900 mb-8">
         Cursos
     </h2>
-    <div className="w-full lg:w-1/2 bg-white p-4 rounded-lg shadow-sm border border-gray-100 mb-8">
-      <h3 className="text-xl font-bold text-gray-900 mb-6">Búsqueda</h3>
-      <SearchBar valorBusqueda={filtros.busqueda} onSearchChange={handleFilterChange} placeholderString="Buscar por título, autor o tecnología..." />
-    </div>
+    {/*aca va el nuevo div */}
+    <div className="grid grid-cols-1 lg:grid-cols-4 gap-8">
     <div className="lg:col-span-1">
       <CursosFiltros filtrosSeleccionados={filtros} onFiltroChange={handleFilterChange}/>
     </div>
-    <div className="max-w-7xl mx-auto my-8 px-4 sm:px-6 lg:px-8">
+    <div className="lg:col-span-3">
+      <div className='mb-6'>
+        <SearchBar valorBusqueda={filtros.busqueda} onSearchChange={handleFilterChange} placeholderString="Buscar por título, autor o tecnología..." />
+      </div>
       {cargando ? (
         // Estado de carga
         <p className="text-center text-xl text-gray-600">Buscando cursos...</p>
       ) : (
         // Grid de tarjetas usando las clases de Tailwind
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {cursosParaMostrar.map((item) => (
             <div key={item.id}>
               <CursoCard unCurso={item} />
@@ -49,6 +50,9 @@ return (
           ))}
         </div>
       )}
+    </div>
+    </div>
+    <div className="mt-8 flex justify-center">
       <Paginador paginaActual={paginaActual} totalPaginas={totalPaginas} onChangePagina={irALaPagina}/>
     </div>
     </div>
