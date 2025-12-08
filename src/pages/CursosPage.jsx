0accebfecba0d4a0e9462f2bd5b29087b630f244
CristianAlarconDev <calarcon@fi.uba.ar>
2025-12-07 22:03:25 -0300

    se agrega componente filtros mas el hook useCursosFilters, se obtienen cursosFiltrados y ahora renderiza eso en el map; falta corregir estilos y revisar version mobile

--- src/pages/CursosPage.jsx
+++ src/pages/CursosPage.jsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import CursoCard from '../components/CursoCard';
+import SearchBar from '../components/SearchBar';
+import CursosFiltros from '../components/CursosFiltros';
+import { useCursosFilters } from '../hooks/useCursosFilters';
+import { usePaginacion } from '../hooks/usePaginacion'
 import { CursosProvider, useCursosContext } from '../context/CursosContext';
 
 
@@ -12,10 +16,17 @@ const CursosPage = () => {
   obtenerCursos().then(
     setCargando(false))
   },
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   [] );
+  const { filtros, handleFilterChange, cursosFiltrados } = useCursosFilters(cursos);
+  const { datosPaginados: cursosParaMostrar, paginaActual, totalPaginas, irALaPagina }= usePaginacion(cursosFiltrados, 10, filtros);
 
 
 return (
+  <div>
+    <div className="lg:col-span-1">
+            <CursosFiltros filtrosSeleccionados={filtros} onFiltroChange={handleFilterChange}/>
+    </div>
     <div className="max-w-7xl mx-auto my-8 px-4 sm:px-6 lg:px-8">
       {cargando ? (
         // Estado de carga
@@ -23,13 +34,14 @@ return (
       ) : (
         // Grid de tarjetas usando las clases de Tailwind
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-          {cursos.map((item) => (
+          {cursosFiltrados.map((item) => (
             <div key={item.id}>
               <CursoCard unCurso={item} />
             </div>
           ))}
         </div>
       )}
+    </div>
     </div>
   );
 };
