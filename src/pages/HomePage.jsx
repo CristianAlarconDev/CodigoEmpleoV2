9455b112d776094b86e8155ef613bff32da2f0fe
Cristian <calarcon@fi.uba.ar>
2025-12-11 10:17:18 -0300

    correccion de alineacion de card en seccion cursos recientes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -47,7 +47,7 @@ const HomePage = () => {
                       <div className="animate-spin rounded-full h-10 w-10 border-b-2 border-blue-600"></div>
                   </div>
                   ) : (
-                      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-items-center">
+                      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 ">
                           {cursosRecientes.map((curso) => (
                             // _id de Mongo
                             <div key={curso._id}>
