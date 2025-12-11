0034f6febd0211bd499741670b5b689dfb833dce
Cristian <calarcon@fi.uba.ar>
2025-12-11 06:40:02 -0300

    maneja por completo la autenticacion de un usuario con firebase, trae la data con fetch si el usuairo esta en backend si no hace la solicitud para crearlo alla

--- src/context/AutenticacionContext.jsx
+++ src/context/AutenticacionContext.jsx
@@ -6,7 +6,9 @@ const AutenticacionContext = createContext();
 
 export const AutenticacionProvider = ({ children }) => {
     const [usuario, setUsuario] = useState(null);
+    const [cargandoAuth, setCargandoAuth] = useState(true);
     const administradores = import.meta.env.VITE_ADMINISTRADORES || "";
+    const API_URL = import.meta.env.VITE_API_BASE + "/usuarios";
     /*
     const login = (datosUsuario) => {
         setUsuario(datosUsuario);
@@ -20,35 +22,79 @@ export const AutenticacionProvider = ({ children }) => {
         logout
     };
 */
-
     function esAdmin(user){
         if (!user || !user.email) return false;
         return(administradores.includes(user.email))
     }
     
+    
     useEffect(() => {
         /*abro el 'listener' */
-        const unsubscribe = onAuthStateChanged(autenticacion, (usuarioEnFirebase) => {
-            if (usuarioEnFirebase) {
-                const datosUsuario = {
-                    nombre: usuarioEnFirebase.displayName,
-                    email: usuarioEnFirebase.email,
-                    fotoURL: usuarioEnFirebase.photoURL,
-                    uid: usuarioEnFirebase.uid
-                };
-                setUsuario(datosUsuario);
-            } else {
-                setUsuario(null);
+        const unsubscribe = onAuthStateChanged(autenticacion, async(firebaseUser) => {
+        if (firebaseUser) {
+            try {
+                const res = await fetch(`${API_URL}/${firebaseUser.uid}`);
+                if (res.ok) {
+                    // SI ESTA EL USUARIO 
+                    const dbData = await res.json();
+                    
+                    // SE FUSIONA LO DE MONGO DESDE FIREBASE
+                    setUsuario({ ...firebaseUser, ...dbData }); 
+                    console.log(" Usuario cargado desde MongoDB");
+
+                    } else if (res.status === 404) {
+                    // SI NO ESTA LO AGREGAMOS AL BACKEND
+                    console.warn(" Usuario nuevo, registrando en Backend...");
+                        
+                    // TENGO QUE REVISAR QUE COINCIDA CON EL SCHEMA
+                    const nuevoUsuarioBackend = {
+                        firebase_uid: firebaseUser.uid,
+                        email: firebaseUser.email,
+                        nombre: firebaseUser.displayName || "Usuario Nuevo",
+                        imagen: firebaseUser.photoURL, // EN ESPECIAL ESTE CAMPO NO RECUERDO SI ERA IMAGEN
+                        rol: "user",
+                        cursos_guardados: [],
+                        empleos_guardados: [],
+                        postulaciones: []
+                    };
+
+                    const createRes = await fetch(API_URL, {
+                        method: "POST",
+                        headers: { "Content-Type": "application/json" },
+                        body: JSON.stringify(nuevoUsuarioBackend)
+                    });
+
+                    if (createRes.ok) {
+                        const dataCreada = await createRes.json();
+                        setUsuario({ ...firebaseUser, ...dataCreada });
+                        console.log(" Usuario registrado con éxito en DB");
+                    } else {
+                            console.error("Error creando usuario en DB", await createRes.text());
+                        }
+                    }
+
+            } catch (error) {
+                console.error(" Error conectando con el Backend:", error);
+                //DEJO EL USUARIO BASICO DE FIREBAS AUNQUE NO DEBERIA LLEGAR ACA
+                    setUsuario(firebaseUser);
             }
-        });
+            
+        } else {
+            setUsuario(null);
+        }
+            setCargandoAuth(false);
+        }
+    );
         /*cerrar el 'listener', recomendado por si se desomanta seguido el componente;
         mismo funcioamiento */
         return () => unsubscribe();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const logout = () => signOut(autenticacion);
     const data = {
         usuario,
+        cargandoAuth,
         logout,
         esAdmin
     }
