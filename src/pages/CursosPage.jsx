2576f1bda3d5dbda27c33efed2f605700727611c
Cristian <calarcon@fi.uba.ar>
2025-12-10 23:16:34 -0300

    ya no usa context sino el hook useCursos (igual que en empleos) devuevle data, los filtros para los values del componente filtros las acciones que encapsulan los handlers o funciones, la meta para paginacion y busqueda como la keyword para el searchbar

--- src/pages/CursosPage.jsx
+++ src/pages/CursosPage.jsx
@@ -1,27 +1,12 @@
-import React, { useEffect, useState } from 'react'
 import CursoCard from '../components/CursoCard';
 import SearchBar from '../components/SearchBar';
 import CursosFiltros from '../components/CursosFiltros';
 import Paginador from '../components/Paginador';
-import { useCursosFilters } from '../hooks/useCursosFilters';
-import { usePaginacion } from '../hooks/usePaginacion'
-import { CursosProvider, useCursosContext } from '../context/CursosContext';
+import { useCursos } from '../hooks/useCursos.js';
 
 
 const CursosPage = () => {
-
-  const{cursos,obtenerCursos} = useCursosContext();
-  const [cargando, setCargando]=useState(true);
-  
-  useEffect(()=>{
-  obtenerCursos().then(
-    setCargando(false))
-  },
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  [] );
-  const { filtros, handleFilterChange, cursosFiltrados } = useCursosFilters(cursos);
-  const { datosPaginados: cursosParaMostrar, paginaActual, totalPaginas, irALaPagina }= usePaginacion(cursosFiltrados, 10, filtros);
-
+  const { cursos, meta, cargando, busqueda, filtros, acciones } = useCursos();
 
 return (
   <div className="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8">
@@ -31,29 +16,37 @@ return (
     {/*aca va el nuevo div */}
     <div className="grid grid-cols-1 lg:grid-cols-4 gap-8">
     <div className="lg:col-span-1">
-      <CursosFiltros filtrosSeleccionados={filtros} onFiltroChange={handleFilterChange}/>
+      <CursosFiltros filtrosSeleccionados={filtros} onFiltroChange={acciones.filtrar}/>
     </div>
     <div className="lg:col-span-3">
       <div className='mb-6'>
-        <SearchBar valorBusqueda={filtros.busqueda} onSearchChange={handleFilterChange} placeholderString="Buscar por título, autor o tecnología..." />
+        <SearchBar valorBusqueda={busqueda} onSearchChange={(e) => acciones.buscar(e.target.value)} 
+        placeholderString="Buscar por título, autor o tecnología..." />
       </div>
       {cargando ? (
         // Estado de carga
         <p className="text-center text-xl text-gray-600">Buscando cursos...</p>
       ) : (
         // Grid de tarjetas usando las clases de Tailwind
+        <>
+        <div className="mb-4">
+          <p className="text-sm text-gray-500">
+              Se encontraron <span className="font-bold text-gray-800">{meta.total}</span> cursos
+          </p>
+        </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {cursosParaMostrar.map((item) => (
-            <div key={item.id}>
+          {cursos.map((item) => (
+            <div key={item._id}>
               <CursoCard unCurso={item} />
             </div>
           ))}
         </div>
+        </>
       )}
     </div>
     </div>
     <div className="mt-8 flex justify-center">
-      <Paginador paginaActual={paginaActual} totalPaginas={totalPaginas} onChangePagina={irALaPagina}/>
+      <Paginador paginaActual={meta.page} totalPaginas={meta.totalPages} onChangePagina={acciones.cambiarPagina}/>
     </div>
     </div>
   );
